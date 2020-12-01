a919070a87afc2aeb1c0da6ee531ee22c8d3c34f
Gayrat <gena68@inbox.ru>
2020-12-01 21:48:26 +0300

    04. Routes добавлен контроллер User опробовано test route на User@index

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 Курс React+Laravel+Next
 https://www.udemy.com/course/react-laravel-influencer/learn/lecture/22536426?components=deal_badge%2Cdiscount_expiration%2Cgift_this_course%2Cprice_text%2Cpurchase%2Credeem_coupon%2Cslider_menu%2Ccacheable_deal_badge%2Ccacheable_discount_expiration%2Ccacheabl#questions
 
+github:
+https://github.com/antoniopapa
+https://github.com/antoniopapa/admin-backend
+
 composer create-project laravel/laravel laravel-next
 
 docker-compose up
@@ -9,3 +13,10 @@ wsl -l
 
 Сжать диск docker
 Optimize-VHD -Path Y:\dockerData\ext4.vhdx -Mode Full
+
+
+===============
+Создать контроллер (Lara 8)
+php artisan make:controller UserController3
+создал здесь:
+app/Http/Controllers/UserController.php
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+
+Route::get('hello', [\App\Http\Controllers\UserController::class,'indexHelp']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('chart', 'DashboardController@chart');
+    Route::get('user', [\App\Http\Controllers\UserController::class,'user']);
+    Route::put('users/info', [\App\Http\Controllers\UserController::class,'updateInfo']);
+    Route::put('users/password', [\App\Http\Controllers\UserController::class,'updatePassword']);
+    Route::post('upload', 'ImageController@upload');
+    Route::get('export','OrderController@export');
+
+    Route::apiResource('users', 'UserController');
+    Route::apiResource('roles', 'RoleController');
+    Route::apiResource('products', 'ProductController');
+    Route::apiResource('orders', 'OrderController')->only('index', 'show');
+    Route::apiResource('permissions', 'PermissionController')->only('index');
+});
+
